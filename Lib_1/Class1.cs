8236c7deb8aea826bd3188db53acc98f1869494d
MichaelCPP4 <michaelplay3452@gmail.com>
2022-09-29 23:41:58 +0300

    Добавьте файлы проекта.

---
+++ Lib_1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_1
+{
+    public class Matematik
+    {
+        static public void SschetChisel (int k, out string chisla, out int sum)
+        {
+            sum = 0;
+            int a;
+            chisla = "0";
+            Random r = new Random();
+
+            while (sum<k)
+            {
+                a = r.Next(2, 11);
+                chisla = a.ToString();
+                sum += a;
+            }
+        }
+    }
+}
